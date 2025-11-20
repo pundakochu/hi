d480fcb3d441faacfa949e9204a6dd8eee2d6fc8
pundakochu <pundakochu5@gmail.com>
2025-11-20 01:25:34 -0800

    Add files via upload

---
+++ hi.py
@@ -0,0 +1,5 @@
+ Connection-specific DNS Suffix  . :
+   Link-local IPv6 Address . . . . . : fe80::7b2a:337:299b:cf9d%5
+   IPv4 Address. . . . . . . . . . . : 192.168.1.101
+   Subnet Mask . . . . . . . . . . . : 255.255.255.0
+   Default Gateway . . . . . . . . . : 192.168.1.1
